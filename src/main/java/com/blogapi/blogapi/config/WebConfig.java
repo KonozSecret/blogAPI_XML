3a4a7ca2848564d899fee4eaf5c3be89f49f86c7
konozsecret <nabutsu19@gmail.com>
2024-05-21 09:44:47 +0200

    test

--- src/main/java/com/blogapi/blogapi/config/WebConfig.java
+++ src/main/java/com/blogapi/blogapi/config/WebConfig.java
@@ -1,3 +1,5 @@
+package com.blogapi.blogapi.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
